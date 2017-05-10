9f1cb626167c2226c7ac9c824f2393993a438166
Andrey Shmakov <roll.loko@mail.ru>
2017-05-10 22:27:50 +0300

    Главная страница авторизованного пользователя

--- application/controllers/Feed.php
+++ application/controllers/Feed.php
@@ -1,14 +1,25 @@
 <?php
 
-class Welcome extends CI_Controller
+class Feed extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
+        $this->load->library("twig");
+        $this->load->library('session');
+        $this->load->helper('url');
     }
 
     public function index()
     {
-        echo '<a href="https://oauth.vk.com/authorize?client_id=5240196&display=page&redirect_uri=http://blog.loc/user/vk_auth&response_type=code&v=5.64">Авторизация</a>';
+        // Если авторизован пользовтаель показываем ему {feed} иначе {welcome}
+        if (isset($this->session->user["uid"])) {
+
+            echo $this->twig->render("feed.php", array("user" => $this->session->user, "title" => "Главная"));
+
+        } else {
+
+            echo $this->twig->render("welcome.php", array("title" => "Подтверждение входа", "base_url" => base_url()));
+        }
     }
 }
